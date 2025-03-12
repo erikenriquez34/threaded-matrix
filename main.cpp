0075f1877258ab096d3c9780ddd0c0964ef68f69
Erik Enriquez <erikenriquez34@gmail.com>
2025-03-12 19:34:44 -0400

    Verbose mode option

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include <vector>
 #include <chrono>
 #include <unistd.h>
+#include <thread>
 
 using namespace std;
 using namespace std::chrono;
 
 using Matrix = vector<vector<int>>;
+bool verbose = false;
 
-Matrix transpose(const Matrix& A) {
+Matrix transpose_sequential(const Matrix& A) {
+    if (verbose) printf("[VERBOSE] Starting sequential matrix transpose.\n");
     Matrix B(A[0].size(), vector<int>(A.size()));
     for (int i = 0; i < A.size(); i++) {
         for (int j = 0; j < A[0].size(); j++) {
             B[j][i] = A[i][j];
         }
     }
+    if (verbose) printf("[VERBOSE] Tranpose complete.\n");
     return B;
 }
 
@@ -34,12 +38,13 @@ void printHelp() {
     printf("Options:\n");
     printf("  -m <rows>   Specify the number of rows in the matrix.\n");
     printf("  -n <cols>   Specify the number of columns in the matrix.\n");
+    printf("  -v          Enable verbose mode.\n");
     printf("  -h          Display this help message.\n");
 }
 
 int main(int argc, char* argv[]) {
     int opt, m, n;
-    while ((opt = getopt(argc, argv, "m:n:h")) != -1) {
+    while ((opt = getopt(argc, argv, "hm:n:v")) != -1) {
         switch(opt) {
             case 'h':
                 printHelp();
@@ -50,6 +55,9 @@ int main(int argc, char* argv[]) {
             case 'n':
                 n = atoi(optarg);
                 break;
+            case 'v':
+                verbose = true;
+                break;
             case ':':
                 printf("Missing argument for option: %c\n", (char)optopt);
                 return 1;
@@ -59,16 +67,26 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    if (m <= 0 || n <= 0) {
+        printf("Error: Matrix dimensions must be positive integers.\n");
+        return 1;
+    }
+
+    if (verbose) printf("[VERBOSE] Creating a %d by %d matrix.\n", m, n);
     Matrix A(m, vector<int>(n));
     for (int i = 0; i < A.size(); i++) {
         for (int j = 0; j < A[0].size(); j++) {
             A[i][j] = i * A.size() + j;
         }
     }
+    if (verbose) printf("[VERBOSE] Done.\n");
 
+    //will do this multiple times for all methods
     auto start = high_resolution_clock::now();
-    Matrix B = transpose(A);
+    Matrix B = transpose_sequential(A);
     auto stop = high_resolution_clock::now();
+
+    if (verbose) printf("[VERBOSE] Verify valid transpose: %s\n", is_transpose(A, B) ? "true" : "false");
 
     auto duration = duration_cast<milliseconds>(stop - start);
     printf("Sequential: %d ms\n", duration.count());
